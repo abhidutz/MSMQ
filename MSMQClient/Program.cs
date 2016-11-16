b2862afd6f3b0474d97ba4207e6466e3a1a3281b
abhin <abhin@LAPTOP-0VANCD6Q>
2016-11-16 01:16:23 -0500

    Add project files.

---
+++ MSMQClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSMQClient.MSMQServiceReference;
+namespace MSMQClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MSMQServiceClient proxy = new MSMQServiceClient();
+            for(int i = 0; i < 100; i++) {
+                proxy.GetData("hello"+i);
+                Console.WriteLine(i+ " Message sent.");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
