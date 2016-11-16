b8b696034bcb1a17cf63e25724e4cbdcd2bb0c91
abhidutz <abhinay.nitj@gmail.com>
2016-11-16 01:27:46 -0500

    Add files via upload

---
+++ MSMQService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MSMQNoSecurityService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MSMQService : IMSMQService
+    {
+        public void GetData(string msg)
+        {
+            Debug.WriteLine(msg + "Received at:" + System.DateTime.Now.ToString());
+        }
+
+
+    }
+}
